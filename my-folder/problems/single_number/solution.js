d500add090269a3ab9c264e30acd2422bf0118a2
Kevin Liu <30753677+kliu33@users.noreply.github.com>
2023-08-23 18:03:55 +0000

    Sync LeetCode submission - Single Number (javascript)

---
+++ my-folder/problems/single_number/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let set1 = new Set();
+    for (let num of nums){
+        if (set1.has(num)){
+            set1.delete(num)
+        } else {
+            set1.add(num)
+        }
+    }
+    return Array.from(set1)[0]
+};
